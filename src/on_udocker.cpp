b803eb8518c9bffde45707c45002135adbd53b37
Nguyen Van Hiep <hiep12092001@gmail.com>
2021-05-02 09:05:52 -0700

    push🎅🛒

---
+++ src/on_udocker.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	system("docker run -it -d --name udocker udocker:latest");
+	return 0;
+}
---
+++ src/udocker.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	system("docker exec -it udocker /bin/bash");
+	return 0;
+}
